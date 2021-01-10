772aed7a52d2e02a1bb35219e46aaec6990ea567
samiscold <62956581+samiscold@users.noreply.github.com>
2021-01-10 14:46:27 +0100

    slider

--- js/images.js
+++ js/images.js
@@ -1,6 +1,9 @@
 var i = 0;
 var images = []; 
 var time = 3000; 
+var leftButton = document.getElementsByClassName('left');
+var rightButton = document.getElementsByClassName('right');
+var index = 0;
 
 
 images[0] = "img/ubt-studentat.jpg";
@@ -9,18 +12,25 @@ images[2] = "img/ubt-slide2.jpg";
 images[3] = "img/ubt-slide-4.jpg";
 
 
+//auto change
 function changeImg() {
 	document.slide.src = images[i];
-
-	if (i < images.length - 1) {
-		i++;
-	} else {
-		i = 0;
-	}
-
-
-	setTimeout("changeImg()", time);
 }
+
+// on clicks
+
+function prev() {
+	if(i <= 0 ) i = images.length;
+	i--;
+	return changeImg();
+}
+
+function next() {
+	if(i >= images.length - 1 ) i = -1
+	i++;
+	return changeImg();
+}
+
 
 
 window.onload = changeImg;
